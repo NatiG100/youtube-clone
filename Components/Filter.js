89afb2ce80ce3f2d4e12696a235dc653e34df7fb
NatiG100 <kalebok39@gmail.com>
2022-07-01 06:42:23 -0700

    fix slider to hide buttons when reaching edge

--- Components/Filter.js
+++ Components/Filter.js
@@ -1,14 +1,33 @@
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import { Box, Chip, IconButton, Typography } from "@mui/material";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Filter= ()=>{
     const divRef = useRef();
+    const [start,setStart] = useState(false);
+    const [end,setEnd] = useState(false);
+    function updateSliderState(){
+        if(divRef.current.scrollLeft===0){
+            setStart(true);
+            setEnd(false);
+        }else if(divRef.current.scrollLeft===divRef.current.scrollWidth-divRef.current.clientWidth){
+            setEnd(true);
+            setStart(false);
+        }else{
+            setEnd(false);
+            setStart(false);
+        }
+    }
+    useEffect(()=>{
+        updateSliderState();
+    },[])
     function moveRight(){
         divRef.current.scrollLeft -= 160;
+        updateSliderState();
     }
     function moveLeft(){
         divRef.current.scrollLeft += 160;
+        updateSliderState();
     }
 
     const filters=[
@@ -36,14 +55,14 @@ const Filter= ()=>{
     ]
     return(
         <Box width="100%" position="relative" height="max-content" overflow="hidden">
-            <Slider direction="left" onClick={moveRight}/>
-            <Box className="hide-scroll" ref={divRef}  width={"100%"} display="flex" overflow={"auto"} gap="1%" py={2} px={11} borderTop="1px solid #393939" borderBottom={"1px solid #393939"} sx={{backgroundColor:"#202020",scrollbarWidth:"none"}}>
+            {!start&&<Slider direction="left" onClick={moveRight}/>}
+            <Box className="hide-scroll" ref={divRef}  width={"100%"} display="flex" overflow={"auto"} gap="1%" py={2} px={4} borderTop="1px solid #393939" borderBottom={"1px solid #393939"} sx={{backgroundColor:"#202020",scrollbarWidth:"none"}}>
                 {filters.map((item,index)=>(
                     <Chip key={index} label={item} onClick={()=>{}}  sx={{border:"1px solid #555555",fontSize:"14px",fontWeight:"600",color:"#dadada",backgroundColor:"#323232"}}/>
                 ))}
                 
             </Box>
-            <Slider direction="right" onClick={moveLeft}/>
+            {!end&&<Slider direction="right" onClick={moveLeft}/>}
         </Box>
     );
 }
@@ -52,7 +71,7 @@ export default Filter;
 const Slider = ({direction="left",onClick=()=>{}})=>{
     return(
         <Box  my={"1px"} zIndex={"1000"} position={"absolute"} left={direction==="left"?"0px":""} right={direction!=="left"?"0px":""} top="0px"display="flex" justifyContent="center" alignItems="center" height="100%" px={3} sx={{
-            backgroundImage: `linear-gradient(${direction==="left"?"to right":"to left"},#202020 50%, #0000)`,
+            backgroundImage: `linear-gradient(${direction==="left"?"to right":"to left"},#202020 70%, #0000)`,
         }}>
             <IconButton onClick={onClick}>
                 {direction==="left"?<ChevronLeft/>:<ChevronRight/>}
